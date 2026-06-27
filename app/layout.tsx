3fdd867954e2ea0a141cdad745286565559a67d7
Rin Nguyen <nrin31266@gmail.com>
2026-06-27 20:50:33 +0700

    up phan tich

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Be_Vietnam_Pro } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/next"
 const beVietnamPro = Be_Vietnam_Pro({
   subsets: ["latin", "vietnamese"],
   weight: ["400", "500", "600", "700"],
@@ -14,6 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="vi" className={beVietnamPro.variable}>
+      <Analytics/>
       <body>{children}</body>
     </html>
   );
